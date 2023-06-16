0eea6dd573b6059bc26c93910030d3ff1c861979
monicah79 <monniewanjema@gmail.com>
2023-06-16 15:38:06 -0700

    Loop through a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
