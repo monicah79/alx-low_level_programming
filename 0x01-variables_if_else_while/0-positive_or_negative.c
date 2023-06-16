1eaa79f6657132059b0a58bd0e823aa17c5b9a93
monicah79 <monniewanjema@gmail.com>
2023-06-16 12:44:38 -0700

    Positive and nevative if statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero", n);
+	} else
+	{
+		printf("%d is positive", n);
+	}
+	return (0);
+}
