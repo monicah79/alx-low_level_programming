8d0f76a143f0685db60b6263dea6229d8633c612
monicah79 <monniewanjema@gmail.com>
2023-06-16 21:26:21 -0700

    11 commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i +  1; j <= 9; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				if (i == j || j == k || i == k)
+				{
+					continue;
+				}
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i == 7 && j == 8 && k == 9)
+				{
+					break;
+				}
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
