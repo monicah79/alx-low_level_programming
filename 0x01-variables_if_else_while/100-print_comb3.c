3619722424c398c298e04d4c20148080d5b5cf36
monicah79 <monniewanjema@gmail.com>
2023-06-16 20:43:14 -0700

    10commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-  for(int i = 0; i <= 9; i++) {
-    for(int j = i+1; j <= 9; j++) {
-      if(i == j) {
-        continue;
-      }
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-      putchar(i+'0');
-      putchar(j+'0');
-      
-      if(i == 8 && j == 9) {
-        break;
-      }
-      
-      putchar(',');
-      putchar(' ');
-    }
-  }
-  putchar('\n');
+int main(void)
+{
+	int i, j;
+
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = i + 1; j <= 9; j++)
+		{
+			if (i == j)
+			{
+				continue;
+			}
+
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i == 8 && j == 9)
+			{
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
