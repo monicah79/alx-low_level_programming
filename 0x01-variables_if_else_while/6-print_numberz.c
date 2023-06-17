9b0e68f5ca4a7e9839eea6f3cf1810dfb337bdb0
monicah79 <monniewanjema@gmail.com>
2023-06-16 18:33:09 -0700

    6 commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+}
