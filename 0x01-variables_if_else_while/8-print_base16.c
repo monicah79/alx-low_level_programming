2526443fc28e7d7bb900910cac1f9cfc54b8bcb6
monicah79 <monniewanjema@gmail.com>
2023-06-16 19:52:13 -0700

    commit 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+	int e;
+
+	for (d = 48; d < 58; d++)
+	{
+		putchar(d);
+	}
+	for (e = 'a'; e < 'g'; e++)
+	{
+		putchar(e);
+	}
+	putchar('\n');
+}
