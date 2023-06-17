b0a29b80a9888e242b7d32b22b02bbf5735942b8
monicah79 <monniewanjema@gmail.com>
2023-06-16 18:58:03 -0700

    last commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+			for (int k = 0; k <= 9; k++)
+			{
+				for (int l = 0; l <= 9; l++)
+				{
+					if (i == j && j == k && k == l)
+					{
+						continue;
+					}
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+
+					if (i == 9 && j == 8 && k == 9 && l == 9)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
