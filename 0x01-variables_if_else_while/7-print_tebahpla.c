b44d5e3bf892f2b398da0a7cf170b36eaf4a78fe
monicah79 <monniewanjema@gmail.com>
2023-06-16 19:30:23 -0700

    7 commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
