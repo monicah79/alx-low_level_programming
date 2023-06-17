948daabf749b3ca711d7d075eb8cf2e2504f03fa
monicah79 <monniewanjema@gmail.com>
2023-06-16 18:23:58 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int y  = 0;
+
+	while (y < 10)
+	{
+		printf("%d", y);
+		y++
+	}
+	printf("\n");
+	return (0);
+}
