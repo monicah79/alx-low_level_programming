3211b7bac64c25e1fa036b4335a1941d47d813b7
monicah79 <monniewanjema@gmail.com>
2023-06-20 15:44:24 -0700

    sum of 2 interger

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,8 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+void print_to_98(int n);
 #endif
