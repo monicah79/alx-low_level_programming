bae066f4aa660d3694cb2ae1db05ec8eca5edb46
monicah79 <monniewanjema@gmail.com>
2023-06-20 13:02:20 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* print_alphabet - alphabet is printed
+*
+* Return: Always 0
+*/
 
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+        char a;
+
+        for (a = 'a'; a <= 'z'; a++)
+        {
+                _putchar(a);
+        }
+        _putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,32 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * _putchar - check the code
- *
- * Return: Always 0.
- */
-
-int _putchar(void)
-{
-	return (0);
-}
-
-/**
- * print_alphabet - check the code
- *
- * Return: nothing.
- */
-
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
-}
-
-#endif /* MAIN_H */
+int _putchar(char c);
+void print_alphabet(void);
+#endif
