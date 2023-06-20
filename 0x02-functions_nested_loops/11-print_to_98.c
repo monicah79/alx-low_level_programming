40d754d2d8970fe697581ace6cddab1d752f32f8
monicah79 <monniewanjema@gmail.com>
2023-06-20 16:17:16 -0700

    last comit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,29 +10,28 @@
 
 void print_to_98(int n)
 {
-        int i;
+	int i;
 
-        if (n <= 98)
-        {
-                for (i = n; i < 98; i++)
-                {
-                        _putchar(i / 10 + '0');
-                        _putchar(i % 10 + '0');
-                        _putchar(',');
-                        _putchar(' ');
-                }
-        }
-        else
-        {
-                for (i = n; i > 98; i--)
-                {
-                        _putchar(i / 10 + '0');
-                        _putchar(i % 10 + '0');
-                        _putchar(',');
-                        _putchar(' ');
-                }
-        }
-        _putchar('9');
-        _putchar('8');
-        _putchar('\n');
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	} else
+	{
+		for (i = n; i > 98; i--)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
