9e1ea52f67dc0a7e708328f0ce5e1547fe7f0ac9
monicah79 <monniewanjema@gmail.com>
2023-06-21 19:22:29 -0700

    12 commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,48 @@
 #include "main.h"
+
+/**
+ * print_times_table - check the code.
+ *
+ * @n: type integer
+ *
+ * Return: Always 0.
+ */
+
 void print_times_table(int n)
 {
+	int i, j, product;
+
 	if (n < 0 || n > 15)
 	{
 		return;
 	}
-	for (int i = 0; i <= 10; i++)
+	for (i = 0; i <= n; i++)
 	{
-		int result = i * n;
-		int divisor = 10;
-		
-		if (result > 9)
+		for (j = 0; j <= n; j++)
 		{
-			divisor = 100;
+			product = i * j;
+
+			if (product < 10)
+			{
+				putchar(' ');
+				putchar(' ');
+				putchar(' ');
+				putchar(product + '0');
+			} else if
+				(product < 100)
+				{
+					putchar(' ');
+					putchar(' ');
+					putchar(product / 10 + '0');
+					putchar(product % 10 + '0');
+				} else
+				{
+					putchar(' ');
+					putchar(product / 100 + '0');
+					putchar((product / 10) % 10 + '0');
+					putchar(product % 10 + '0');
+				}
 		}
-		if (result > 99)
-		{
-			divisor = 1000;
-		}
-		putchar(i + '0');
-		putchar(' ');
-		putchar('x');
-		putchar(' ');
-		putchar(n + '0');
-		putchar(' ');
-		putchar('=');
-		putchar(' ');
-		if (result == 0)
-		{
-			putchar('0');
-		} else
-		{
-			while (divisor > 0) {
-                int digit = result / divisor;
-                putchar(digit + '0');
-                result -= digit * divisor;
-                divisor /= 10;
-            }
-        }
-        putchar('\n');
-    }
+		putchar('\n');
+	}
 }
