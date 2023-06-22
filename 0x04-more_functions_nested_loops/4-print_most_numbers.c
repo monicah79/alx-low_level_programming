3be8e012c810aa06ec6a72bce3d8d8e0f9110788
monicah79 <monniewanjema@gmail.com>
2023-06-22 10:22:49 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code.
+ *
+ *
+ * Return: Always 0.
+ */
+
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i != 2) && (i != 9))
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
