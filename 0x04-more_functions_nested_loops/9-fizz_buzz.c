b2165afff270e7089bd8757c611e1a80428d3c30
monicah79 <monniewanjema@gmail.com>
2023-06-22 15:14:31 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 100; x++)
+	{
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("fizzbuzz");
+		} else if
+		(x % 3 == 0)
+		{
+			printf("Fizz");
+		} else if
+		(x % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		printf("%d ", x);
+	}
+	printf("\n");
+	return (0);
+}
