7b1ccda9e1cc8d2844f9a33c16f9399475447036
monicah79 <monniewanjema@gmail.com>
2023-06-22 06:04:42 -0700

    check Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code.
+ *
+ * @c: return integer
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && C <= 'Z';)
+	{
+		return (1);
+	}
+	return (0);
+}
