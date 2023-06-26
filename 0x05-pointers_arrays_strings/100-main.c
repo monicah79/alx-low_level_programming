a66ecf8eb3f0952c4b542f3b20f3a809e4218afc
monicah79 <monniewanjema@gmail.com>
2023-06-26 16:07:00 -0700

    last commit

--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -22,7 +22,7 @@ int main(void)
 	printf("%d\n", nb);
 	nb = _atoi("Suite 402");
 	printf("%d\n", nb);
-	nb = _atoi("        +     +   -   -98 Battery Street; San Francisco, CA 94111 - USA");
+	nb = _atoi("  +   +   -   -98 Battery Street; San Francisco, CA 94111 - USA");
 	printf("%d\n", nb);
 	nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
 	printf("%d\n", nb);
