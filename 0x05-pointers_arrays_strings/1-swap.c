036c8f3744a2971cba16eaf7ee76fbf1b3627baf
monicah79 <monniewanjema@gmail.com>
2023-06-26 08:25:04 -0700

    Swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *
+ * @a: return value of temp
+ * @b: return value of a
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*b = *a;
+	*a = temp;
+}
