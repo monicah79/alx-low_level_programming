d4fb0e80eb092f9935e4bfa974bb37422f6d3ee3
monicah79 <monniewanjema@gmail.com>
2023-06-26 07:55:43 -0700

    Pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - check the code
+ *
+ * @n: define and int
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Welcome to pointers and arrays
