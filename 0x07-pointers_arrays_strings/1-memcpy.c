b5bcbb6b0ba34d2eaa2fcd9a8f60601a1762a4c2
monicah79 <monniewanjema@gmail.com>
2023-07-03 16:19:37 -0700

    Last commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,9 +10,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	while (n--)
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
-		*dest++ = *src++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
