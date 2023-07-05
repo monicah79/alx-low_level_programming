ca6b702a0047bc0aaafb0874636dd6c170a7b685
monicah79 <monniewanjema@gmail.com>
2023-07-05 07:46:10 -0700

    first commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ * @s: print string
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
