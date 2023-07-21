5c4b03951e7256197e1ec85a95c45073c81e36e0
monicah79 <monniewanjema@gmail.com>
2023-07-20 17:11:16 -0700

    Array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef function_pointers_h
 #define function_pointers_h
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
