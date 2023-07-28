e73fa008289a11201a9bc626af4cec71df2bad8e
monicah79 <monniewanjema@gmail.com>
2023-07-27 18:09:34 -0700

    Correcting errorrs

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,12 @@ size_t print_list(const list_t *h)
 {
 	size_t len = 0;
 
-	if (h == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
-
 	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		len++;
 	}
