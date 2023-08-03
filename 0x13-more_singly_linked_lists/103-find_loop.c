bf363e587a88efe9ebb4e047a20e0d4bbe7a27f7
monicah79 <monniewanjema@gmail.com>
2023-08-02 18:47:18 -0700

    Fix betty

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,26 +10,24 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *slow = head, *fast = head;
+	listint_t *slow = head, *fast = head;
 
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-        {
-            slow = head;
+		if (slow == fast)
+		{
+			slow = head;
 
-            while (slow && fast && slow != fast)
-            {
-                slow = slow->next;
-                fast = fast->next;
-            }
-
-            return (slow);
-        }
-    }
-
-    return (NULL);
+			while (slow && fast && slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+	return (NULL);
 }
